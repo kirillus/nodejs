600725dec27a26874646d5bab5acc3443f8ea0ee
kirillus <amaga@me.com>
2020-06-20 11:16:19 -0400

    init 2

---
+++ routes/api/posts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/posts
+// @desc    Test route
+// @access  Private
+router.get('/', (req, res) => {
+  res.send('Posts Router');
+});
+
+module.exports = router;
